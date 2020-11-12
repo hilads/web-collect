a0487f1904033a49789eca0eaca3493b0b86c68c
gaoliang <1356074096@qq.com>
2020-11-12 18:43:59 +0800

    g

---
+++ reduce.js
@@ -0,0 +1,61 @@
+let m = [{
+        a: '一年级',
+        b: '小红'
+    },
+    {
+        a: '二年级',
+        b: '小黑'
+    },
+    {
+        a: '一年级',
+        b: '小绿'
+    },
+    {
+        a: '三年级',
+        b: '小资'
+    },
+    {
+        a: '二年级',
+        b: '小冯'
+    },
+    {
+        a: '四年级',
+        b: 'tony'
+    },
+]
+// 一
+m.reduce(
+    (arr, {
+        a,
+        b
+    }) =>
+    ((arr[a] || (arr[a] = [])).push(b), arr), {})
+console.log(m);
+
+// 二
+m.reduce(
+    (arr, {
+        a,
+        b
+    }) => {
+        (arr[a] ? arr[a] : arr[a] = []).push(b), arr
+    }, {})
+// 三
+m.reduce(
+    (arr, {
+        a,
+        b
+    }) => {
+        (arr[a] ? arr[a] : arr[a] = []).push(b)
+        return arr;
+    }, {})
+
+
+
+// 逗号操作符
+function myFunc() {
+    var x = 0;
+    console.log((x += 1, x))
+    return (x += 1, x); // the same of return ++x;
+
+}
